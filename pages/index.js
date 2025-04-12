5121d3e101c3efa11a58b45eae1f02825ee2056c
Emmanuel Pereira Pires <emmanuelf988@gmail.com>
2025-04-12 02:21:04 -0300

    Create README.md

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Hello World!</h1>;
+    return <h1>Oi Aline, Você é incrível! <br/> Continue brilhando! <br/> Se você me ama dá um sorriso! 😊  </h1>;
 }
 
 export default Home;
