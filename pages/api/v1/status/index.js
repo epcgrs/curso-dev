378706e6c5ca8f7a3494db0463c40848a4f816ab
epcgrs <emmanuelf988@gmail.com>
2025-04-18 02:04:22 -0300

    adicionando arquivos database e .env

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status(request, response) {
+import database from '../../../../infra/database.js'
+
+async function status(request, response) {
+  const result = await database.query('SELECT 1 + 1 AS two');
+  console.log(result.rows[0].two); // 2
+  
   response.status(200).json({
     status: "ok",
   });
