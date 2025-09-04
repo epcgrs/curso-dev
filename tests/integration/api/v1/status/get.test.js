21381fe3841c1845c51cbce48759262b0d21d6ba
epcgrs <emmanuelf988@gmail.com>
2025-09-04 18:07:12 -0300

    positive test error

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -15,7 +15,7 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
   expect(responseBody.postgress_version).toBeDefined();
-  expect(responseBody.postgress_version).toMatch(/PostgreSQL/);
+  expect(responseBody.postgress_version).toMatch(/PostgreSQLite/);
   expect(typeof responseBody.postgress_version).toBe('string');
 
   expect(responseBody.max_connections).toBeDefined();
